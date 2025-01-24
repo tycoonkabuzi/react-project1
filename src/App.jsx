b010d03c48ded5da25eaf7e5c0e8478af0ada5f1
Tycoon Kabuzi <kabuzitycoon@gmail.com>
2025-01-24 14:49:17 +0100

    Work done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import "./App.css";
-import { useState } from "react";
 import Counter from "./components/counter";
 
 function App() {
-  const [count, setCount] = useState();
-
   return (
     <>
       <div className="header">
-        <h1>Hello world</h1>
+        <h1>Counter app</h1>
       </div>
-      <Counter counterValue={count} changedValue={setCount} />
+      <Counter />
     </>
   );
 }
